60e35f88dd2aa0dcbb0517f98352193878a6a872
RianbowJier <3021809270@qq.com>
2024-06-24 20:59:47 +0800

    :lipstick: SQL: 初始化会员中心数据库表结构及插入初始数据
    创建了member表，并定义了id、用户名、密码、手机号码、邮箱和性别等字段。
    同时，插入了三条初始会员数据。此次变更旨在为E-Commerce-Center项目提供基础的会员信息存储方案。

---
+++ SQL/E-Commerce-Center.sql
@@ -0,0 +1,29 @@
+create table member
+(
+    id     bigint auto_increment comment 'id'
+        primary key,
+    NAME   varchar(64) null comment '用户名',
+    pwd    varchar(32) null comment '密码',
+    mobile varchar(20) null comment '手机号码',
+    email  varchar(64) null comment '邮箱',
+    gender tinyint     null comment '性别'
+);
+
+
+create table member
+(
+    id     bigint auto_increment comment 'id'
+        primary key,
+    NAME   varchar(64) null comment '用户名',
+    pwd    varchar(32) null comment '密码',
+    mobile varchar(20) null comment '手机号码',
+    email  varchar(64) null comment '邮箱',
+    gender tinyint     null comment '性别'
+);
+
+INSERT INTO e_commerce_center_db.member (id, NAME, pwd, mobile, email, gender)
+VALUES (1, 'smith', '123', '123456789000', 'smith@sohu.com', 0);
+INSERT INTO e_commerce_center_db.member (id, NAME, pwd, mobile, email, gender)
+VALUES (2, 'RainbowJier', '112233', 'test', 'test', 1);
+INSERT INTO e_commerce_center_db.member (id, NAME, pwd, mobile, email, gender)
+VALUES (7, 'test', 'test', 'test', 'test', 0);
