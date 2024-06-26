477d7df498807b5e73d53867236f1e45300a8f5a
RianbowJier <3021809270@qq.com>
2024-06-26 20:19:34 +0800

    feat(member-openfeign-consumer): 添加OpenFeign日志配置和超时设置

---
+++ E-Commerce-Center/member-openfeign-consumer-80/src/main/java/com/example/memberopenfeignconsumer80/config/OpenFeignLogConf.java
@@ -0,0 +1,21 @@
+package com.example.memberopenfeignconsumer80.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description：OpenFeign日志配置
+ * @Author： RainbowJier
+ * @Data： 2024/6/26 19:42
+ */
+
+@Configuration
+public class OpenFeignLogConf {
+
+    @Bean
+    Logger.Level loggerLevel() {
+        //日志级别指定为FULL
+        return Logger.Level.FULL;
+    }
+}
